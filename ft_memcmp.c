db43816f91e79e28426a9c976e5c8c3a7e010e0d
Silas Rafe <sirafe@e3r6p6.21-school.ru>
2018-11-28 17:36:31 +0300

    v2

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: sirafe <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 15:13:22 by sirafe            #+#    #+#             */
-/*   Updated: 2018/11/28 15:18:15 by sirafe           ###   ########.fr       */
+/*   Updated: 2018/11/28 17:13:38 by sirafe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,18 @@
 
 int			ft_memcmp(const void *a1, const void *a2, size_t n)
 {
-	const unsigned char		*ptr;
-	const unsigned char		*ptr1;
-	size_t						i;
+	size_t				i;
+	const unsigned char	*p1;
+	const unsigned char	*p2;
 
-	ptr = a1;
-	ptr1 = a2;
+	p1 = s1;
+	p2 = s2;
 	i = 0;
-	while (ptr[i] != '\0' && ptr1[i] != '\0' && i < n)
+	while (i < n)
 	{
-		if (ptr[i] != ptr1[i])
-			return (ptr[i] - ptr1[i]);
+		if (p1[i] != p2[i])
+			return (p1[i] - p2[i]);
 		i++;
 	}
-	if ((ptr[i] == '\0' && ptr1[i] != '\0') || (ptr[i] == '\0' &&
-												ptr1[i] != '\0'))
-		return (ptr[i] - ptr1[i]);
 	return (0);
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,21 +6,23 @@
 /*   By: sirafe <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 15:43:04 by sirafe            #+#    #+#             */
-/*   Updated: 2018/11/28 15:46:46 by sirafe           ###   ########.fr       */
+/*   Updated: 2018/11/28 17:33:36 by sirafe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *str1, const char *str2, size_t n)
+int		ft_strncmp(const char *str1, const char *str2, size_t n)
 {
-	while (n != 0)
+	size_t	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (str1[i] == str2[i] && str1[i] != '\0' && n > 0)
 	{
-		if (*str1 != *str2)
-			return (*(unsigned char*)(str1 - 1) - *(unsigned char*)(str2 - 1));
+		i++;
 		n--;
-		str1++;
-		str2++;
 	}
-	return (0);
+	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
 }
