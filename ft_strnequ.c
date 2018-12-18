3dfaac9c67b3033b2b0bf897683b2ab135ad9b05
Silas Rafe <sirafe@e3r6p6.21-school.ru>
2018-12-18 19:05:00 +0300

    last

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: sirafe <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 19:38:00 by sirafe            #+#    #+#             */
-/*   Updated: 2018/12/12 18:38:43 by sirafe           ###   ########.fr       */
+/*   Updated: 2018/12/18 18:39:10 by sirafe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,7 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	if (s1 && s2 && n)
-	{
-		if (ft_strncmp(s1, s2, n) == 0)
-			return (1);
-		else
-			return (0);
-	}
-	else
-	{
-		if (*s2 == '\0')
-			return (1);
-		else
-			return (-1);
-	}
+	if (s1 && s2)
+		return (ft_strncmp(s1, s2, n) == 0);
+	return (0);
 }
